3b70642b795974573e00cf99fdcce6a817da7d37
Harshvardhan Singh <hvsreal.223@gmail.com>
2022-04-26 16:54:53 +0530

    added: new base url

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,2 +1,4 @@
-export const BASE_URL = "https://api-alascafashion.herokuapp.com/api/v1/";
+// export const BASE_URL = "https://api-alascafashion.herokuapp.com/api/v1/";
+// export const BASE_URL = "https://alascafashion-api.vercel.app/api/v1/";
+export const BASE_URL = "http://64.227.130.229:8080/api/v1/";
 // export const BASE_URL = "http://localhost:8080/api/v1/";
